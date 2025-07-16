ba8c0118ff9e60425b74d645dff2664a721b7074
MANPRIT SINGH <83352826+manpritsingh-mod@users.noreply.github.com>
2025-07-16 12:15:44 +0530

    Update core_utils.groovy

--- vars/core_utils.groovy
+++ vars/core_utils.groovy
@@ -82,6 +82,7 @@ def readProjectConfig(){
             // Check if readYaml is available (Pipeline Utility Steps plugin)
             if (this.metaClass.respondsTo(this, 'readYaml')) {
                 config = readYaml file: configFile
+                Logger.info("Config map content: ${config}")
                 Logger.info("Configuration loaded from ${configFile}")
             } else {
                 Logger.warning("Pipeline Utility Steps plugin not installed - cannot read YAML files")
